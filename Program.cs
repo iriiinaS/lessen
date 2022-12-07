393d0c24bb429e6d9be10a2a061e58de58639f40
Irina <irina.sorokina.97@bk.ru>
2022-12-07 10:41:46 +0300

    z15

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /*  Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+     */
+
+
+int a;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine ()!, out a);
+string num = a.ToString();
+
+if (num.Length < 3)
+    Console.Write($"Третьей цифры нет");
+else
+{
+    Console.Write($"{a}) -> {num [2]}");
+}
